125583c9e6cdcf83950b89b01cef6e84df99974f
leticiademartinobou <121428742+leticiademartinobou@users.noreply.github.com>
2023-05-16 12:34:47 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+let dividend = x;
+let divisor = y;
+let result = dividend % divisor;
+
+const letraDNI = ('T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E');
+const numeroResto = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23);
+const letraNumero = (T===0, R===1, W===2, A===3, G===4, M===5, Y===6, F===7, P===8, D===9, X===10, B===11, N===12, J===13, Z===14, S===15, Q===16, V===17, H===18, L===19, C===20, K===21, E===22);
+
+for(let i=0; i<=23; i++) {
+    if (result === numeroResto)
+    return letraNumero
+}
